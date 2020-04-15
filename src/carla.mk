806f6ff0f82f86ee22f5af595da804f18a8c896a
Alexandros Theodotou <alex@zrythm.org>
2020-04-15 15:50:32 +0100

    use github for carla

--- src/carla.mk
+++ src/carla.mk
@@ -1,12 +1,12 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := carla
-$(PKG)_WEBSITE  := https://github.com/falktx/Carla
+$(PKG)_WEBSITE  := https://github.com/falkTX/Carla
 $(PKG)_DESCR    := Carla
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 5c5d8c1cebce131ea08432dfed64fa54a4a477fa
+$(PKG)_VERSION  := 5c5d8c1
 $(PKG)_CHECKSUM := 8f85f7b68a7c70dde2161841f35cc9cec035a3b28a3ef9adce1def9c31ffaeec
-$(PKG)_SUBDIR   := Carla-$($(PKG)_VERSION)
+$(PKG)_GH_CONF  := falkTX/Carla/branches/develop
 $(PKG)_FILE     := Carla-$($(PKG)_VERSION).zip
 $(PKG)_URL      := https://github.com/falkTX/Carla/archive/$($(PKG)_VERSION).zip
 $(PKG)_DEPS     := cc libsndfile
