7dbac510935f3b9599ad6b01b7dd6665399d9087
Alexandros Theodotou <alex@zrythm.org>
2020-06-11 20:44:13 +0100

    fix carla build

--- src/carla.mk
+++ src/carla.mk
@@ -12,7 +12,8 @@ $(PKG)_DEPS     := cc libsndfile fluidsynth carla-32-bin
 define $(PKG)_BUILD
 	cd '$(SOURCE_DIR)' && \
 		$(SED) -i -e "s|NATIVE_LINK_FLAGS) -o|NATIVE_LINK_FLAGS) -L$(PREFIX)/$(TARGET)/lib -lintl -o|g" source/plugin/Makefile source/bridges-plugin/Makefile && \
-		$(SED) -i -e "s|\t@|\t|g" source/bridges-plugin/Makefile source/plugin/Makefile
+		$(SED) -i -e "s|\t@|\t|g" source/bridges-plugin/Makefile source/plugin/Makefile && \
+		$(SED) -i -e "s| wine | touch |g" source/plugin/Makefile
 	cd '$(SOURCE_DIR)' && $(MAKE) -j '$(JOBS)' \
 		CC=$(subst shared,static,$(TARGET))-gcc CXX=$(subst shared,static,$(TARGET))-g++ \
 		PKG_CONFIG=$(subst shared,static,$(TARGET))-pkg-config BUILDING_FOR_WINDOWS=true
