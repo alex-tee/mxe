5b1a8134f5e5c1e9aa61a5fc03e1d7e3873f2801
Alexandros Theodotou <alex@zrythm.org>
2020-06-12 17:52:45 +0100

    fix carla build

--- src/carla.mk
+++ src/carla.mk
@@ -13,7 +13,8 @@ define $(PKG)_BUILD
 	cd '$(SOURCE_DIR)' && \
 		$(SED) -i -e "s|NATIVE_LINK_FLAGS) -o|NATIVE_LINK_FLAGS) -L$(PREFIX)/$(TARGET)/lib -lintl -o|g" source/plugin/Makefile source/bridges-plugin/Makefile && \
 		$(SED) -i -e "s|\t@|\t|g" source/bridges-plugin/Makefile source/plugin/Makefile && \
-		$(SED) -i -e "s| wine | touch |g" source/plugin/Makefile
+		$(SED) -i -e "s| wine | touch |g" source/plugin/Makefile && \
+		$(SED) -i -e 's|bin/carla.lv2/\*.ttl| |' Makefile
 	cd '$(SOURCE_DIR)' && $(MAKE) -j '$(JOBS)' \
 		CC=$(subst shared,static,$(TARGET))-gcc CXX=$(subst shared,static,$(TARGET))-g++ \
 		PKG_CONFIG=$(subst shared,static,$(TARGET))-pkg-config BUILDING_FOR_WINDOWS=true
