02de17f11380bcf9d1781790f19e6bf7d7298896
Alexandros Theodotou <alex@zrythm.org>
2021-01-02 07:46:51 +0000

    update carla

--- src/carla.mk
+++ src/carla.mk
@@ -4,8 +4,8 @@ PKG             := carla
 $(PKG)_WEBSITE  := https://github.com/falkTX/Carla
 $(PKG)_DESCR    := Carla
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := d8c7f92
-$(PKG)_CHECKSUM := 942761851303322540fa564e46c34c0739a58394378bd2eea57278f7473d20d3
+$(PKG)_VERSION  := ba75822
+$(PKG)_CHECKSUM := e6bb7673862f4974e1d171230b1651a2ad22ace07fce809c18d906c945a54e4a
 $(PKG)_GH_CONF  := falkTX/Carla/branches/develop
 $(PKG)_DEPS     := cc libsndfile fluidsynth carla-32-bin
 
